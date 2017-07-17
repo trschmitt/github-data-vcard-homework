d12b2aa5607e46200ea75a1a4b969a25ae640d87
Tristan Schmitt <trschmitt50@live.com>
2017-07-17 14:53:26 -0400

    started JS file

---
+++ main.js
@@ -0,0 +1,6 @@
+let header = document.querySelector("#name");
+let headerContent = document.createElement("h1")
+let theBasics = document.querySelector("#the-basics");
+let theStory = document.querySelector("#the-story");
+
+document.
