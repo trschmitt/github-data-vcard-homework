7a19293f2c7140905966f7d5b31d5cb1fb6d222d
Tristan Schmitt <trschmitt50@live.com>
2017-07-17 15:42:00 -0400

    template almost finished

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-let header = document.querySelector("#name");
-let headerContent = document.createElement("h1")
-let theBasics = document.querySelector("#the-basics");
-let theStory = document.querySelector("#the-story");
+let container = document.querySelector("#container");
 
-document.
+let request = new XMLHttpRequest();
+request.open("GET", "https://api.github.com/users/trschmitt");
+request.addEventListener("load", githubInfo);
+request.send();
+
+function businessCard() {
+  let data = JSON.parse(this.responseText)
+  let template = `
+  <header id="name">
+  <h1>${data.name}</h1>
+  </header>
+  <section id="the-basics">
+    <h2>The Basics</h2>
+    <ul>
+      <li>${data.name}</li>
+      <li>${data.html_url}</li>
+      <li>${data.company}</li>
+      <li>${data.blog}</li>
+    </ul>
+  </section>
+  <section id="the-story">
+    <h2>The Story</h2>
+    <p>bio</p>
+    <img src="" alt="">
+  </section>
+  `
+}
