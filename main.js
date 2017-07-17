49ea0357cba840539de517c46f035e26e45b13b0
Tristan Schmitt <trschmitt50@live.com>
2017-07-17 16:30:21 -0400

    tinkering with CSS, not finished.

--- main.js
+++ main.js
@@ -10,29 +10,31 @@ function businessCard() {
   let data = JSON.parse(this.responseText);
 
   let template = `
-  <header id="name">
+  <header>
     <h1>${data.name}</h1>
   </header>
-  <section id="the-basics">
-    <div>
-      <h2>The Basics</h2>
-    </div>
-    <ul>
-      <li>${data.name}</li>
-      <li>${data.html_url}</li>
-      <li>${data.company}</li>
-      <li>${data.blog}</li>
-    </ul>
-  </section>
-  <section id="the-story">
-    <h2>The Story</h2>
-    <div>
-      <p>${data.bio}</p>
-    </div>
-    <div id="picture">
-      <img src=${data.avatar_url} alt="pro_picture">
-    </div>
-  </section>
+  <div class="content">
+    <section id="the-basics">
+      <div>
+        <h2>The Basics</h2>
+      </div>
+      <ul>
+        <li><span>Name: </span>${data.name}</li>
+        <li><span>URL: </span>${data.html_url}</li>
+        <li><span>Company: </span>${data.company}</li>
+        <li><span>Website: </span>${data.blog}</li>
+      </ul>
+    </section>
+    <section id="the-story">
+      <h2>The Story</h2>
+      <div>
+        <p>${data.bio}</p>
+      </div>
+      <div id="picture">
+        <img src=${data.avatar_url} alt="pro_picture">
+      </div>
+    </section>
+  </div>
   `
   container.innerHTML = template;
 }
